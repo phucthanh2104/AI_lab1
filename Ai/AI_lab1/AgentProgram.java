7215bb5b8b7cd7de8a3e3a8d6c96cc00628533d8
phucthanh2104 <145191722+phucthanh2104@users.noreply.github.com>
2023-10-23 12:47:13 +0700

    lab 2 3

---
+++ Ai/AI_lab1/AgentProgram.java
@@ -0,0 +1,22 @@
+package chapter2.agent_AB;
+
+import javax.tools.DocumentationTool.Location;
+
+import chapter2.agent_AB.Environment.LocationState;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		// TODO
+		if (p.getLocationState() == LocationState.DIRTY) {
+			return Environment.SUCK_DIRT;
+		} else if (p.getAgentLocation() == Environment.LOCATION_A) {
+			return Environment.MOVE_RIGHT;
+		} else if (p.getAgentLocation() == Environment.LOCATION_B) {
+			return Environment.MOVE_LEFT;
+		}
+
+		return NoOpAction.NO_OP;
+
+	}
+}
